cdbdcd276f3c660bdf5550180a292e84466bd47b
Jakob Schlyter <jakob@kirei.se>
2021-03-24 09:56:35 +0100

    reformat

--- examples/movie_xmas.py
+++ examples/movie_xmas.py
@@ -5,9 +5,9 @@ import random
 
 from ttls.client import TwinklyFrame
 
-RED = (0xff, 0x00, 0x00)
-GREEN = (0x00, 0xff, 0x00)
-BLUE = (0x00, 0x00, 0xff)
+RED = (0xFF, 0x00, 0x00)
+GREEN = (0x00, 0xFF, 0x00)
+BLUE = (0x00, 0x00, 0xFF)
 
 
 def generate_xmas_frame(n: int) -> TwinklyFrame:
@@ -23,26 +23,32 @@ def generate_xmas_frame(n: int) -> TwinklyFrame:
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument('--leds',
-                        dest='leds',
-                        metavar='n',
-                        type=int,
-                        default=105,
-                        required=False,
-                        help="Number of LEDs")
-    parser.add_argument('--count',
-                        dest='count',
-                        metavar='n',
-                        type=int,
-                        default=10,
-                        required=False,
-                        help="Number of iterations")
-    parser.add_argument('--output',
-                        dest='output',
-                        metavar='filename',
-                        type=str,
-                        default='movie.bin',
-                        help="Output file")
+    parser.add_argument(
+        "--leds",
+        dest="leds",
+        metavar="n",
+        type=int,
+        default=105,
+        required=False,
+        help="Number of LEDs",
+    )
+    parser.add_argument(
+        "--count",
+        dest="count",
+        metavar="n",
+        type=int,
+        default=10,
+        required=False,
+        help="Number of iterations",
+    )
+    parser.add_argument(
+        "--output",
+        dest="output",
+        metavar="filename",
+        type=str,
+        default="movie.bin",
+        help="Output file",
+    )
     args = parser.parse_args()
 
     movie = []
@@ -51,7 +57,7 @@ def main() -> None:
         frame = [v for sublist in generate_xmas_frame(args.leds) for v in sublist]
         movie.extend(frame)
 
-    with open(args.output, 'wb') as f:
+    with open(args.output, "wb") as f:
         f.write(bytes(movie))
 
 
--- examples/realtime_xmas.py
+++ examples/realtime_xmas.py
@@ -1,15 +1,15 @@
 """Example of sending realtime frames to Twinkly"""
 
-import asyncio
 import argparse
+import asyncio
 import random
 import time
 
 from ttls.client import Twinkly, TwinklyFrame
 
-RED = (0xff, 0x00, 0x00)
-GREEN = (0x00, 0xff, 0x00)
-BLUE = (0x00, 0x00, 0xff)
+RED = (0xFF, 0x00, 0x00)
+GREEN = (0x00, 0xFF, 0x00)
+BLUE = (0x00, 0x00, 0xFF)
 
 
 def generate_xmas_frame(n: int) -> TwinklyFrame:
@@ -25,29 +25,32 @@ def generate_xmas_frame(n: int) -> TwinklyFrame:
 
 async def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument('--host',
-                        metavar='hostname',
-                        required=True,
-                        help='Device address')
-    parser.add_argument('--count',
-                        dest='count',
-                        metavar='n',
-                        type=int,
-                        default=10,
-                        required=False,
-                        help="Number of iterations")
-    parser.add_argument('--delay',
-                        dest='delay',
-                        metavar='seconds',
-                        type=float,
-                        default=0.2,
-                        required=False,
-                        help="Delay between frames")
+    parser.add_argument(
+        "--host", metavar="hostname", required=True, help="Device address"
+    )
+    parser.add_argument(
+        "--count",
+        dest="count",
+        metavar="n",
+        type=int,
+        default=10,
+        required=False,
+        help="Number of iterations",
+    )
+    parser.add_argument(
+        "--delay",
+        dest="delay",
+        metavar="seconds",
+        type=float,
+        default=0.2,
+        required=False,
+        help="Delay between frames",
+    )
     args = parser.parse_args()
 
     t = Twinkly(host=args.host)
     await t.interview()
-    await t.set_mode('rt')
+    await t.set_mode("rt")
 
     for _ in range(0, args.count):
         frame = generate_xmas_frame(t.length)
