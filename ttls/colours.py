a040dc9fc5482ce84f003258c3e0f0daf0867a84
Jakob Schlyter <jakob@kirei.se>
2022-12-12 13:14:46 +0100

    clean up

--- ttls/colours.py
+++ ttls/colours.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
-from typing import Optional, Tuple, Union
+from typing import Dict, Optional, Tuple, Union
 
 ColourTuple = Union[Tuple[int, int, int], Tuple[int, int, int, int]]
+ColourDict = Dict[str, int]
 
 
 @dataclass(frozen=True)
@@ -13,20 +14,30 @@ class TwinklyColour:
 
     def as_twinkly_tuple(self) -> ColourTuple:
         """Convert TwinklyColour to a tuple as used by Twinkly: (R,G,B) or (W,R,G,B)"""
-        return (
-            (self.w, self.r, self.g, self.b)
-            if self.w is not None
-            else (self.r, self.g, self.b)
-        )
+        if self.w is not None:
+            return (self.w, self.r, self.g, self.b)
+        else:
+            return (self.r, self.g, self.b)
 
     def as_tuple(self) -> ColourTuple:
         """Convert TwinklyColour to a tuple: (R,G,B) or (R,G,B,W)"""
-        return (
-            (self.r, self.g, self.b, self.w)
-            if self.w is not None
-            else (self.r, self.g, self.b)
-        )
+        if self.w is not None:
+            return (self.r, self.g, self.b, self.w)
+        else:
+            return (self.r, self.g, self.b)
 
     def __iter__(self):
         for i in self.as_tuple():
             yield i
+
+    def as_dict(self) -> ColourDict:
+        """Convert TwinklyColour to a dict wth color names used by set-led functions."""
+        if self.w is not None:
+            return {
+                "red": self.r,
+                "green": self.g,
+                "blue": self.b,
+                "white": self.w,
+            }
+        else:
+            return {"red": self.r, "green": self.g, "blue": self.b}
